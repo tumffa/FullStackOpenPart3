4b6e15090414b26e590cb3d447e3d1cdd2703db2
tumffa <tuomas.v.niemi@gmail.com>
2024-08-24 17:44:20 +0300

    ex3.4

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
   }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+  })
   
 
 const PORT = 3001
