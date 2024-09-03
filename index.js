dcc9ef21b343d1f2feb189f14fbe62189a14065f
tumffa <tuomas.v.niemi@gmail.com>
2024-09-03 18:57:55 +0300

    ex3.13 - 3.14

--- index.js
+++ index.js
@@ -1,58 +1,48 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
-morgan.token('body', (request) => JSON.stringify(request.body))
-const ownFormat = ':method :url :status :res[content-length] - :response-time ms :body'
+const Person = require('./models/person')
+const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(express.static('dist'))
+const ownFormat = ':method :url :status :res[content-length] - :response-time ms :body'
 app.use(morgan(ownFormat))
 
-let persons = [
-    {
-      id: "1",
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: "2",
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: "3",
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-      id: "4",
-      name: "Mary Poppendieck",
-      number: "39-23-6423122"
-    }
-]
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+morgan.token('body', (request) => JSON.stringify(request.body))
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-    const date = new Date()
+  const date = new Date()
+  Person.find({}).then(persons => {
     response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${date}</p>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${date}</p>
     `)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } 
-  else {
-    response.status(404).end()
-  }
+  Person.findById(id).then(person => {
+    if (person) {
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -77,21 +67,14 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: String(Math.floor(Math.random() * 10000))
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
   
 
