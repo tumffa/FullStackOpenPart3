3502b663ee975bdb9f28428f5718b71277cdbee6
tumffa <tuomas.v.niemi@gmail.com>
2024-09-03 16:56:13 +0300

    ex3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@puhelinluettelo.2h72p.mongodb.net/?retryWrites=true&w=majority&appName=puhelinluettelo`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('error connecting to MongoDB:', error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 5) {
+  const newName = process.argv[3];
+  const newNumber = process.argv[4];
+  const newPerson = new Person({
+    name: newName,
+    number: newNumber,
+  });
+
+  newPerson.save().then(() => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+}
