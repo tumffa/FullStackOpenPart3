74489ab072a67bc485625368021ee60a40f7bddf
tumffa <tuomas.v.niemi@gmail.com>
2024-10-09 19:05:43 +0300

    ex3.15

--- index.js
+++ index.js
@@ -47,9 +47,10 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(id)
+  .then(result => {
+    response.status(204).end()
+  })
 })
 
 app.post('/api/persons', (request, response) => {
