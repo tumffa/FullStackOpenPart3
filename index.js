432fc12b33d24ff37f8673766c33beff7ff603ae
tumffa <tuomas.v.niemi@gmail.com>
2024-10-09 20:33:17 +0300

    ex3.21

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan')
 const app = express()
 const Person = require('./models/person')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 const ownFormat = ':method :url :status :res[content-length] - :response-time ms :body'
